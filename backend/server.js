692fd4169492a4993e52b873a393cf85e4bb4bf1
Chris K <Chris-SourceCode@outlook.com>
2023-02-18 21:35:30 +0100

    feat[backend]: Add node server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const http = require('http');
+const process = require('process');
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send({ message: 'Hello' });
+});
+
+const sock = process.argv[2];
+
+fs.stat(sock, (err) => {
+  if (!err) { fs.unlinkSync(sock); }
+  http.createServer(app).listen(sock, () => {
+    fs.chmodSync(sock, '777');
+    console.log(`Express server listening on ${sock}`);
+  });
+});
