621dfe2e6a4f37c4af9fed289b0c3d6d9dc05505
Chris K <Chris-SourceCode@outlook.com>
2023-02-19 20:22:27 +0100

    refactor[frontend]: Temporary frontend change

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,38 +12,38 @@ function useDockerDesktopClient() {
 }
 
 export function App() {
-  const [response, setResponse] = React.useState<string>();
+  const [response, setResponse] = React.useState<string>('test');
   const ddClient = useDockerDesktopClient();
 
   const fetchAndDisplayResponse = async () => {
     const result = await ddClient.extension.vm?.service?.get('/hello');
-    setResponse(JSON.stringify(result));
+    setResponse('hello');
   };
 
   return (
     <>
-      <Typography variant="h3">Docker extension demo</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+      <Typography variant='h3'>Docker extension demo</Typography>
+      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
         This is a basic page rendered with MUI, using Docker's theme. Read the
         MUI documentation to learn more. Using MUI in a conventional way and
         avoiding custom styling will help make sure your extension continues to
         look great as Docker's theme evolves.
       </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
         Pressing the below button will trigger a request to the backend. Its
         response will appear in the textarea.
       </Typography>
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
+      <Stack direction='row' alignItems='start' spacing={2} sx={{ mt: 4 }}>
+        <Button variant='contained' onClick={fetchAndDisplayResponse}>
           Call backend
         </Button>
 
         <TextField
-          label="Backend response"
+          label='Backend response'
           sx={{ width: 480 }}
           disabled
           multiline
-          variant="outlined"
+          variant='outlined'
           minRows={5}
           value={response ?? ''}
         />
