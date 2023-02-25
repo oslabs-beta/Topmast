1130643eceeb79fdbb2bc139e7e02c6787ec671d
bacarlsson86 <bacarlsson@gmail.com>
2023-02-25 15:24:57 -0800

    can fetch initial containers

--- backend/logGetter.js
+++ backend/logGetter.js
@@ -1,43 +1,57 @@
 // use the docker engine API to fetch containers and their logs
+// this was originally intended as backend middleware, so that's
+// why it's creating an object to export as a module
+
+// i am not great at async, so be savage if you need to Chris :-)
+
+// docker enginer API reference, 1.42 is current
+// https://docs.docker.com/engine/api/v1.42/
 
 const axios = require('axios');
 
 logGetter = {};
-ids = [];
 logs = {};
+containers = {};
 
-logGetter.getContainerIds = async (req, res, next) => {
+//write this so that only new containers are added
+logGetter.updateContainers = async (containers) => {
   // run a get request on the docker engine daemon
   // that returns all containers
-  try{
+  try {
+    //by default the daemon returns running containers, the all=true
+    //parameter returns all of them
     await axios.get('/containers/json?all=true', {
       socketPath: '/var/run/docker.sock',
     })
-    .then((containers) => {
-      containers.data.forEach(container => {
-        ids.push(container.Id);
+    .then((res) => {
+      // res.data is our array of containers
+      res.data.forEach(container => {
+        // only add new containers
+        // #TODO update changes in container state
+        if (containers[container.Id] === undefined){
+          containers[container.Id] = container;
+        }
       })
-    });
-    } catch (error) {
-      console.log('error in fetching container ids');
-      console.log(error.response);
+    })
+  } catch (error) {
+    console.log('error in fetching container ids');
+    console.log(error);
   }
+  return containers;
   
-  // axios.get(`/containers/${ids[0]}/stats?stream=false`, {
-  //   socketPath: '/var/run/docker.sock',
-  // }).then((stats) => {console.log(stats);})
+}
 
-  // grab each log
+logGetter.getLogs = async (containers) => {
   try {
+    let ids = Object.keys(containers);
     ids.forEach(id => {
-      // do we use stderr or stdout?
+      // i think you use the parameter follow=true to keep a stream open
+      //
+      // how do we handle a stream for each container?
       axios.get(`/containers/${id}/logs?stderr=true`, {
         socketPath: '/var/run/docker.sock',
       })
       .then((log) => {
-        console.log(log);
-        // console.log(log);
-        // console.log(log.data);
         logs[id] = log;
       })
     });
@@ -45,25 +59,17 @@ logGetter.getContainerIds = async (req, res, next) => {
     console.log('error in fetching logs');
     console.log(error.response);
   }
-
-  console.log(logs);
-}
-  // .then((containers) => {
-  //   for (let container in containers.data){
-  //     console.log(container.id);
-  //     ids.push(container.data.id);
-  //   }
-  //   console.log(ids);
-  // })
   
-  // disable when using locally
-  // next();
+}
 
+// stats is similar to the above. here's the start of it. stream=true
+// keeps the connection open
 
+// await axios.get(`/containers/${ids}/stats?stream=false`, {
+//   socketPath: '/var/run/docker.sock'})
 
-logGetter.getContainerIds();
-// logGetter.getLogs();
+// just a test function
+containers = logGetter.updateContainers(containers);
 
-// getContainers();
 
 module.exports = logGetter;
