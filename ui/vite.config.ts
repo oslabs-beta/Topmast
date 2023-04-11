788eb8b1f438e5b19ff181debc2259882cab89f0
ryan <gauserr@icloud.com>
2023-04-10 22:17:03 -0400

    vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
   server: {
     port: 3000,
     strictPort: true,
-  }
-});
+  },
+})
