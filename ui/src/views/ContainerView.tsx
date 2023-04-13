b6d138d526722341786da6c6ae8f2e8bb860c2e2
Jason Brown <superbunker@gmail.com>
2023-04-12 19:36:00 -0700

    routes working

--- ui/src/views/ContainerView.tsx
+++ ui/src/views/ContainerView.tsx
@@ -1,7 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function ContainerView() {
     return (
       <div>
         <h2>Container</h2>
+
+        <div>This is the content on the container view component</div>
+
+        <Link to="/">Link to Dashboard (/)</Link>
       </div>
     );
   }
