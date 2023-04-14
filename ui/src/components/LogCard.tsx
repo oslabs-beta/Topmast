25cfadc3f5d24368fe268e3ab745034fe9a17315
Michael Ruiz <maruiz.sc1@gmail.com>
2023-04-13 19:09:00 -0700

    Combined separate container datagrids into one parent datagrid to display all logs from all containers

--- ui/src/components/LogCard.tsx
+++ ui/src/components/LogCard.tsx
@@ -3,17 +3,6 @@
 // import { Typography } from '@mui/material';
 // import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
-// const rows: GridRowsProp = [
-//   { id: 1, col1: 'Hello', col2: 'World' },
-//   { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-//   { id: 3, col1: 'MUI', col2: 'is Amazing' },
-// ];
-
-// const columns: GridColDef[] = [
-//   { field: 'col1', headerName: 'Column 1', width: 150 },
-//   { field: 'col2', headerName: 'Column 2', width: 150 },
-// ];
-
 // const LogCard = ({ containerId = '', output = [], errors = [] }) => {
 //   // console.log({ containerId, output, errors });
 //   return (
--- ui/src/components/LogsDashboard.tsx
+++ ui/src/components/LogsDashboard.tsx
@@ -1,25 +1,124 @@
+import React, { useState } from 'react';
 import { useAppContext } from '../context/AppContext';
-import { Box } from '@mui/material';
-import LogCard from './LogCard';
+import { Box, Typography, Checkbox, FormControlLabel } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const LogsDashboard = () => {
-  const { logs } = useAppContext();
+  // Get logs and containers from AppContext
+  const { logs, containers } = useAppContext();
+  // Initialize selectedContainers state as an empty set
+  const [selectedContainers, setSelectedContainers] = useState<Set<string>>(
+    new Set()
+  );
 
+  // Define handleCheckboxChange function that updates the selectedContainers state
+  const handleCheckboxChange = (containerId: string, checked: boolean) => {
+    // Create a new set from selectedContainers
+    const newSelectedContainers = new Set(selectedContainers);
+    // If checked is true, add containerId to the new set
+    // Else, delete containerId from the new set
+    checked
+      ? newSelectedContainers.add(containerId)
+      : newSelectedContainers.delete(containerId);
+    // Update selectedContainers state with the new set
+    setSelectedContainers(newSelectedContainers);
+  };
+
+  // Define combinedRows, which will contain the logs to display in the DataGrid
+  // - Use Object.entries() to transform logs object into an array of [containerId, logs] pairs
+  // - Filter this array to only include logs whose containerId is in selectedContainers,
+  //   or include all logs if selectedContainers is empty
+  // - Use flatMap() to create a new array of log rows, with each row including the containerId,
+  //   log type (Output/Error), and log content
+  const combinedRows = Object.entries(logs)
+    .filter(
+      ([containerId]) =>
+        selectedContainers.size === 0 || selectedContainers.has(containerId)
+    )
+    .flatMap(([containerId, { output, errors }]: any[]) => {
+      // Check if output and errors are defined, if not, set them to empty arrays
+      output = output || [];
+      errors = errors || [];
+
+      // For each container, create outputRows from output logs
+      const outputRows = output.map((line, index) => {
+        return {
+          id: `${containerId}-o${index}`,
+          containerId,
+          type: 'Output',
+          content: line,
+        };
+      });
+
+      // For each container, create errorRows from error logs
+      const errorRows = errors.map((line, index) => {
+        return {
+          id: `${containerId}-e${index}`,
+          containerId,
+          type: 'Error',
+          content: line,
+        };
+      });
+
+      // Combine outputRows and errorRows into a single array
+      return [...outputRows, ...errorRows];
+    });
+
+  // Define columns for the DataGrid
+  const columns: GridColDef[] = [
+    { field: 'containerId', headerName: 'Container ID', width: 150 },
+    { field: 'type', headerName: 'Type', width: 100 },
+    {
+      field: 'content',
+      headerName: 'Content',
+      flex: 1,
+      renderCell: (params) => {
+        // Split log content into separate lines
+        const lines = params.value.split('\n');
+        // Render each line of log content as a Typography component
+        return (
+          <div>
+            {lines.map((line, index) => (
+              <Typography key={index}>{line}</Typography>
+            ))}
+          </div>
+        );
+      },
+    },
+  ];
+
+  // Render LogsDashboard component
   return (
-    <Box style={{ height: 400, width: '100%' }}>
-      {Object.entries(logs).map(
-        ([containerId, { output, errors }]: any[], index: number) => {
-          return (
-            <LogCard
-              key={index}
-              containerId={containerId}
-              output={output}
-              errors={errors}
-            />
-          );
-        }
-      )}
-    </Box>
+    <div style={{ height: '100vh', width: '100%' }}>
+      {/* Display a title for the container selection section */}
+      <Typography variant='h6'>Select containers:</Typography>
+      {/* Render container checkboxes */}
+      <Box
+        display='flex'
+        flexWrap='wrap'>
+        {containers.map((container: any) => (
+          <FormControlLabel
+            key={container.ID}
+            control={
+              <Checkbox
+                checked={selectedContainers.has(container.ID)}
+                onChange={(event) =>
+                  handleCheckboxChange(container.ID, event.target.checked)
+                }
+              />
+            }
+            label={container.ID}
+          />
+        ))}
+      </Box>
+      {/* Render the DataGrid with the combinedRows and columns */}
+      <Box style={{ height: '90vh', width: '100%' }}>
+        <DataGrid
+          rows={combinedRows}
+          columns={columns}
+        />
+      </Box>
+    </div>
   );
 };
 
